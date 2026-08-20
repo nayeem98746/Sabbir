12e6fa366661fa56ed84265729788aeb4d575bbe
MD :  Sabbir Ahmed <sabbirahmednayeem941@gmail.com>
2026-08-20 11:07:30 +0600

    changes

--- src/components/routes/home/ShowOffSites.tsx
+++ src/components/routes/home/ShowOffSites.tsx
@@ -9,7 +9,7 @@ const ShowOffSites = () => {
             <h3 className={'homeST.section_title my-8 mt-4'}>Short Shows</h3>
             <div className={'homeST.show_img_wrap flex justify-center'} style={{background:"#1e1e28"}}>
                 <div className={'homeST.show_img w-[80%] rounded-md overflow-hidden'} style={{boxShadow:"0 0  15px 1px #175fa7"}}>
-                    <Image  src={imgUri} width={600} height={300}  layout={"responsive"} alt="Sabbir's websites"></Image>
+                    {/* <Image  src={imgUri} width={600} height={300}  layout={"responsive"} alt="Sabbir's websites"></Image> */}
                 </div>
             </div>
         </section>
